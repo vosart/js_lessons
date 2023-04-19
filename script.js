18e1ed5612da7590e68682c94fcc15d9b7b25139
Artem Voskresensky <metravs@gmail.com>
2023-04-19 16:47:35 +0300

    add posts and button show

--- script.js
+++ script.js
@@ -77,4 +77,54 @@ for (let i = 0; i < choose1.length; i++) {
     choose1[i].addEventListener("click", eventFunc)
 }
 
-choose1[2].removeEventListener('click', eventFunc)
+
+//////////////////////
+const postBlock = document.querySelector('.posts_block-container')
+const showPostsBTN = document.querySelector('.posts_block button') 
+
+function addPost(title, body) {
+    const postTitle = document.createElement('h3')
+    const postBody = document.createElement('span')
+    const postItem = document.createElement('p')
+    
+    postItem.append(postTitle, postBody)
+    postBlock.append(postItem)
+    
+    postTitle.innerText = title
+    postBody.innerText = body
+}
+
+function getPosts() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(data => {
+        for (item of data) {
+            addPost(item.title, item.body)
+        } 
+    })
+    .catch(err => console.log(err.message))
+}
+
+showPostsBTN.onclick = () => {getPosts()}
+
+// function createPost(title, body, userId) {
+//     fetch("https://jsonplaceholder.typicode.com/posts", {
+//         method: 'POST',
+//         body: JSON.stringify({
+//             // title: title,
+//             // body: body,
+//             // userId: userId,
+//             title,
+//             body,
+//             userId,
+//         }),
+//         headers: {
+//             'Content-type': 'application/json; charset=UTF-8',
+//         },
+//     })
+//         .then(res => {
+//             console.log(res)
+//         })
+//         .catch(err => console.log(err.message))
+// }
+// createPost('title', 'body', 17)
